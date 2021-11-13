7c6d5381e4e4d08dc15547802a67d4b0e893d13f
joaopealves <jotalmeida007@hotmail.com>
2021-11-13 18:10:42 -0300

    add profile content

---
+++ src/components/profile/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./profile.css";
+
+export function Profile() {
+  return (
+    <>
+      <div className="ProfileContainer">
+        <div className="AboutMe">
+          <div>
+            <h2>
+              Olá, Eu sou o Juliano, <br />
+              Designer & Software Developer
+            </h2>
+            <p>
+              Cursando Análise e desenvolvimento de Sistemas, curto fazer
+              desenhos, programar e jogar nas horas vagas
+            </p>
+            <button>Download Currículo</button>
+          </div>
+        </div>
+        <div className="Photo">
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
