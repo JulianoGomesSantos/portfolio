2a5309ba38e85aaf7207a62a42b71a88f14e833e
joaopealves <jotalmeida007@hotmail.com>
2021-11-13 18:19:01 -0300

    Updates

---
+++ precache-manifest.9dae96d3bb52c92a21637c8628076f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd7f2fb5944fc670ad813f25f63ab8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56d3c0699b8e2fcc6382",
+    "url": "/static/css/main.2b9083ca.chunk.css"
+  },
+  {
+    "revision": "0e028f488c62eb8f9b08",
+    "url": "/static/js/2.45eeb2eb.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.45eeb2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d3c0699b8e2fcc6382",
+    "url": "/static/js/main.0e8f0fb8.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
