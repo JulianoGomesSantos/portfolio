69b3f88f82c62961b412750c48356d1ec4f7bd04
joaopealves <jotalmeida007@hotmail.com>
2021-11-13 17:10:12 -0300

    Updates

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "./components/navbar";
+import "./styles/home.css";
+
+function App() {
+  return (
+    <div className="HomeContainer">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
