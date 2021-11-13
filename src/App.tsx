9014fd3539c9dc81a7274e503606da405c92bd60
joaopealves <jotalmeida007@hotmail.com>
2021-11-13 17:05:08 -0300

    Update css

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Navbar } from "./components/navbar";
 import "./styles/home.css";
 
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./navbar.css";
 
 export function Navbar() {
