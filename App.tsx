4dfa9db7ca22d163d4257f50bb6691ded0ca3240
joaopealves <jotalmeida007@hotmail.com>
2021-11-13 18:12:26 -0300

    Updates

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Navbar } from "./components/navbar";
+import { Profile } from "./components/profile";
 import "./styles/home.css";
 
 function App() {
   return (
     <div className="HomeContainer">
       <Navbar />
+      <Profile />
     </div>
   );
 }
