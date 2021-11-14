6e896d8c459d1fbf49bf628683525db58c76ea07
joaopealves <jotalmeida007@hotmail.com>
2021-11-13 22:19:49 -0300

    Update css

--- src/components/about_me/index.tsx
+++ src/components/about_me/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { AboutContent } from "../about_content";
 import { Separator } from "../separator";
 
+import { FaTools, FaPencilAlt } from "react-icons/fa";
+import { FiFigma } from "react-icons/fi";
+
 import "./about_me.css";
 
 export function AboutMe() {
@@ -16,19 +19,28 @@ export function AboutMe() {
         <Separator />
         <div className="AboutMeItemsContent">
           <div className="AboutMeItems">
+            <div>
+              <FiFigma />
+              <AboutContent
+                title="DESIGN"
+                text="O Gerador de Lero-lero para TI e informática foi baseado no Fabuloso Gerador de Lero-lero v2.0."
+              />
+            </div>
+            <div>
+              <FaTools />
+              <AboutContent
+                title="DEVELOPMENT"
+                text="O Gerador de Lero-lero para TI e informática foi baseado no Fabuloso Gerador de Lero-lero v2.0."
+              />
+            </div>
+          </div>
+          <div>
+            <FaPencilAlt />
             <AboutContent
-              title="DESIGN"
-              text="O Gerador de Lero-lero para TI e informática foi baseado no Fabuloso Gerador de Lero-lero v2.0."
-            />
-            <AboutContent
-              title="DEVELOPMENT"
+              title="DRAWINGS"
               text="O Gerador de Lero-lero para TI e informática foi baseado no Fabuloso Gerador de Lero-lero v2.0."
             />
           </div>
-          <AboutContent
-            title="DRAWINGS"
-            text="O Gerador de Lero-lero para TI e informática foi baseado no Fabuloso Gerador de Lero-lero v2.0."
-          />
         </div>
       </div>
     </div>
