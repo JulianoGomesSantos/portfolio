7afa98f854c7af7ace83a53a22b11ad65a674ea5
joaopealves <jotalmeida007@hotmail.com>
2021-11-15 11:55:21 -0300

    add modal

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -1,56 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./gallery.css";
+import Modal from "react-modal";
+
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 const { Carousel } = require("react-carousel-minimal");
 
 export function Gallery() {
   const data = [
     {
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+      image: "https://i.imgur.com/jmYUXhy.jpeg",
       caption: "San Francisco",
-    },
-    {
-      image:
-        "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
-      caption: "Scotland",
-    },
-    {
-      image:
-        "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-      caption: "Darjeeling",
-    },
-    {
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-      caption: "San Francisco",
-    },
-    {
-      image:
-        "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-      caption: "Scotland",
-    },
-    {
-      image:
-        "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-      caption: "Darjeeling",
-    },
-    {
-      image:
-        "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-      caption: "San Francisco",
-    },
-    {
-      image:
-        "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-      caption: "Scotland",
-    },
-    {
-      image:
-        "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
-      caption: "Darjeeling",
     },
   ];
+
+  const [clicked, setClicked] = useState(true);
 
   const captionStyle = {
     fontSize: "2em",
@@ -60,14 +24,27 @@ export function Gallery() {
     fontSize: "20px",
     fontWeight: "bold",
   };
+
+  if (clicked) {
+    return (
+      <Modal isOpen={clicked}>
+        <div className="GalleryModalContainer">
+          <button onClick={() => setClicked(false)}>Close</button>
+        </div>
+      </Modal>
+    );
+  }
   return (
     <div className="GalleryContainer">
       <div style={{ textAlign: "center" }}>
         <h2>Galeria</h2>
-        <p>
-          Easy to use, responsive and customizable carousel component for React
-          Projects.
-        </p>
+        <div>
+          <AiOutlinePlusCircle
+            onClick={() => setClicked(true)}
+            size={32}
+            id="addImageIcon"
+          />
+        </div>
         <div
           style={{
             padding: "0 20px",
@@ -91,6 +68,7 @@ export function Gallery() {
             slideImageFit="cover"
             thumbnails={true}
             thumbnailWidth="100px"
+            thumbnailHeight="100px"
             style={{
               textAlign: "center",
               maxWidth: "850px",
