93779e8fcffb5eed0741757669eacc279005dbdc
joaopealves <jotalmeida007@hotmail.com>
2021-11-14 23:40:40 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3e38147b8db5ae2f918ed6997b5a14c4.js"
+  "/portfolio/precache-manifest.10885c3e9403c7808bc7566f8b7c7098.js"
 );
 
 self.addEventListener('message', (event) => {
